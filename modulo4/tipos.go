28026a35afd8ddbde02bb0c0408924c1b71b2872
dornascarol <carolinafdornas@gmail.com>
2024-04-10 20:55:48 -0300

    acrescenta função Printf para os tipos

---
+++ modulo4/tipos.go
@@ -0,0 +1,19 @@
+// TIPOS:
+// bool
+// string
+// int
+// float (float64/float32)
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+	fmt.Printf("Type: %T - Value: %v\n", "steph", "dornas")
+	fmt.Printf("Type: %T - Value: %v\n", 1, 17268)
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1627")
+	fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.557)
+}
