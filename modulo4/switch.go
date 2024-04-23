355867b81e5eb1f2ea5e8284469c08c03fe8f5f3
dornascarol <carolinafdornas@gmail.com>
2024-04-22 23:39:31 -0300

    segundo exemplo de Switch Case

--- modulo4/switch.go
+++ modulo4/switch.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	posicao := 2
+	posicao := 3
 	switch posicao {
 	case 1:
 		fmt.Println("Primeiro lugar")
@@ -11,5 +11,15 @@ func main() {
 		fmt.Println("Segundo lugar")
 	case 3:
 		fmt.Println("Terceiro lugar")
+	}
+
+	nome := "Péricles"
+	switch nome {
+	case "Dornas":
+		fmt.Println("É meu nome")
+	case "Bruce":
+		fmt.Println("É o cachorro da minha amiga")
+	case "Péricles":
+		fmt.Println("É meu cantor brasileiro preferido")
 	}
 }
