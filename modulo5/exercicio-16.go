e125a38cd43e4c9fc8a10052a482203d8bc76bcc
dornascarol <carolinafdornas@gmail.com>
2024-05-14 23:27:30 -0300

    adiciona exercício 16 em Go

---
+++ modulo5/exercicio-16.go
@@ -0,0 +1,24 @@
+// Gopher's Gorgeous Lasagna
+
+package lasagna
+
+// 1)TO DO: define the 'OvenTime' constant
+const OvenTime = 40
+
+// 2)RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+	RemainingOvenTime := OvenTime - actualMinutesInOven
+	return RemainingOvenTime
+}
+
+// 3)PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	PreparationTime := numberOfLayers * 2
+	return PreparationTime
+}
+
+// 4)ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	ElapsedTime := (numberOfLayers * 2) + actualMinutesInOven
+	return ElapsedTime
+}
