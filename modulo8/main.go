7e0141ce17077ea9fd2d87e516939e1c067778ce
dornascarol <carolinafdornas@gmail.com>
2024-05-23 22:29:53 -0300

    acrescenta pacote externo echo e dependências

--- modulo8/main.go
+++ modulo8/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"calculator/math"
 	"fmt"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+
+	// Echo instance
+	e := echo.New()
+
 	soma := math.Soma(3, 7)
 	fmt.Println(soma)
 
