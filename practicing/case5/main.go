faa4e626099745d2680a93958ac2a273566bcf90
dornascarol <carolinafdornas@gmail.com>
2026-02-21 23:34:29 -0300

    add funcs to build list, print list and main

--- practicing/case5/main.go
+++ practicing/case5/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -29,4 +31,43 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 
 	return dummy.Next
+}
+
+func buildList(nums []int) *ListNode {
+	dummy := &ListNode{}
+	current := dummy
+
+	for _, v := range nums {
+		current.Next = &ListNode{Val: v}
+		current = current.Next
+	}
+
+	return dummy.Next
+}
+
+func printList(head *ListNode) {
+	for head != nil {
+		fmt.Print(head.Val)
+		if head.Next != nil {
+			fmt.Print(" -> ")
+		}
+		head = head.Next
+	}
+	fmt.Println()
+}
+
+func main() {
+	l1 := buildList([]int{2, 4, 3})
+	l2 := buildList([]int{5, 6, 4})
+
+	fmt.Print("Lista 1: ")
+	printList(l1)
+
+	fmt.Print("Lista 2: ")
+	printList(l2)
+
+	result := addTwoNumbers(l1, l2)
+
+	fmt.Print("Resultado: ")
+	printList(result)
 }
